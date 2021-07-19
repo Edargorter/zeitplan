d1f542900e5bcdf4b12ed75c7e1e80ac68e6d9f3
Edargorter <zdbowditch777@gmail.com>
2021-07-19 10:47:51 +0200

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # zeitplan
 Mein ganz persönlicher Stundenplan.
+
+## Efficient, minimal, clean 
+
+Zeitplan is to the user what this metaphor is to this project: self-referential.
---
+++ change_filename.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curr_date=$(date "+%T")
+echo $curr_date 
+new_schedule_name=tr -dc A-Za-z0-9 </dev/urandom | head -c 17
+new_php=tr -dc A-Za-z0-9 </dev/urandom | head -c 17
+echo $new_name
+mv $(cat .invis_schedule) $new_schedule_name
+mv $(cat .invis_php) $new_php.php
