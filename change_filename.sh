924a7a77d275d8ef42e3b5009675f05243b4f3c0
Edargorter <zdbowditch777@gmail.com>
2021-07-23 11:31:34 +0200

    Changed filename

--- change_filename.sh
+++ change_filename.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+#Generate new schedule and .php files 
+
 curr_date=$(date "+%T")
 echo $curr_date 
-new_schedule_name=tr -dc A-Za-z0-9 </dev/urandom | head -c 17
-new_php=tr -dc A-Za-z0-9 </dev/urandom | head -c 17
+new_schedule_name=tr -dc A-Za-z0-9 < /dev/urandom | head -c 17
+new_php=tr -dc A-Za-z0-9 < /dev/urandom | head -c 17
 echo $new_name
 mv $(cat .invis_schedule) $new_schedule_name
 mv $(cat .invis_php) $new_php.php
